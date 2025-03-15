c0df2f140f12616eacd7ee348d2bcd75cdf42ee6
Javier Márquez <javier0marquezr@gmail.com>
2025-03-15 21:32:01 +0100

    Primer commit

---
+++ Examenes-C/Abril-2012.c
@@ -0,0 +1,123 @@
+/*
+En este examen se pide:
+Implementar un árbol de búsqueda binario. 
+- Crear 
+- Insertar
+- Recorrer (imprimir nodos)
+- Destruir
+
+(Las interfaces de las funciones ya vienen dadas, y se pide su implementación).
+
+Más abajo hay una explicación del uso de (*arbol)
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Nodo* TArbol;
+struct Nodo {
+    int num;    
+    TArbol izq; // Subárbol izquierdo
+    TArbol der; // Subárbol derecho
+};
+
+// En esta función, como se nos da sin argumento `num`, podemos
+// presuponer que hay que crear un árbol vacío
+void crearAB(TArbol *arbol){
+    *arbol = NULL; // Sí, no hace falta más
+}
+
+/*
+La notación variable->elemento, se utiliza cuando `variable` es un puntero a un struct.
+Si tenemos "un puntero a un puntero a un struct", como TArbol *arbol, 
+hay que hacer (*arbol)->num.
+*/
+void insertarEnABB(TArbol *arbol, int elem) {
+    if (*arbol == NULL) {
+        *arbol = (TArbol)malloc(sizeof(struct Nodo));
+        (*arbol)->num = elem;
+        (*arbol)->izq = NULL;
+        (*arbol)->der = NULL;
+    } else if (elem < (*arbol)->num) {
+        insertarEnABB(&((*arbol)->izq), elem);
+    } else {
+        insertarEnABB(&((*arbol)->der), elem);
+    }
+}
+
+void recorrerABBHelper(TArbol arbol, int *primeroImpreso) {
+    if (arbol != NULL) {
+        recorrerABBHelper(arbol->izq, primeroImpreso);
+        // Si el hijo izquierdo no ha sido NULL, primeroImpreso se habrá
+        // puesto a 1.
+        if (!(*primeroImpreso)) { 
+            printf("%d", arbol->num);
+            *primeroImpreso = 1; // Ya se ha impreso. Usar comas            
+        } else {
+            printf(", %d", arbol->num);
+        }
+        recorrerABBHelper(arbol->der, primeroImpreso);
+    }
+}
+
+void recorrerABB(TArbol arbol) {
+    int impreso = 0; // Variable para guardar si ya hemos impreso el primero
+    recorrerABBHelper(arbol, &impreso);
+}
+
+
+void destruirABB(TArbol *arb) {
+
+}
+
+
+int main() {
+    TArbol arbol;
+
+    crearAB(&arbol);
+    
+    insertarEnABB(&arbol, 1);
+    insertarEnABB(&arbol, 3);
+    insertarEnABB(&arbol, 10);
+    insertarEnABB(&arbol, 10);
+    insertarEnABB(&arbol, 2);
+    insertarEnABB(&arbol, 20);
+    insertarEnABB(&arbol, 2);
+    insertarEnABB(&arbol, 2);
+    
+    recorrerABB(arbol);
+    
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+/*
+Imaginemos que no usamos TArbol *arbol en el argumento de la función, sino:
+
+```
+void crearAB(TArbol arbol){
+    arbol = NULL;
+}
+```
+
+Luego al usarla no tendrá el efecto esperado:
+
+```
+TArbol miArbol;
+void crearAB(TArbol miArbol);
+```
+
+Esto no funcionará como esperamos, y no modificará la variable `miArbol`; no la
+pondrá a NULL. ¿Por qué? Porque se hace una copia del puntero, una copia de la 
+dirección. La función no modifica la dirección (puntero) original.
+
+*/
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Programacion-de-Sistemas-y-Concurrencia
+# Programación de Sistemas y Concurrencia
+Repositorio para la asignatura. Esta asignatura utiliza los lenguajes C y Java.
+
+En este repositorio podrás ver:
+- 
+
+
+**TODO: Tabla explicando contenidos del repositorio. Un índice**
+(TODO: Poner links una vez subido a github)
+
+# Exámenes C
+
+| Fecha         | Contenidos    |
+|---------------|---------------|
+| Abril de 2012 | Árbol binario |
